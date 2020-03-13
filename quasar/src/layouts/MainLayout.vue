c78e82f1d26f5a4623aba0b28e4a9d375c50b482
fabiocardosojr <fabio.cardosojr@gmail.com>
2020-03-13 12:17:55 -0300

    Upload
    Upload inicial

---
+++ quasar/src/layouts/MainLayout.vue
@@ -0,0 +1,100 @@
+<template>
+  <q-layout view="hHh Lpr lff">
+    <q-header elevated>
+      <q-toolbar style = "background-color: #de7ca0">
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+        <q-toolbar-title>
+          Cardápio Digital
+        </q-toolbar-title>
+        <!-- <q-item>
+            <img src="~assets/logo.png" alt="">
+          </q-item> -->
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-1"
+    >
+      <q-list>
+        <q-item-label
+          header
+          class="text-grey-8"
+        >
+          Menu
+        </q-item-label>
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<style>
+  .q-item img {
+    width: 10%;
+    height: auto;
+  }
+</style>
+
+<script>
+import EssentialLink from 'components/EssentialLink';
+
+export default {
+  name: 'MainLayout',
+
+  components: {
+    EssentialLink,
+  },
+
+  data() {
+    return {
+      leftDrawerOpen: false,
+      essentialLinks: [
+        {
+          title: 'Quero Pedir',
+          caption: 'Faça seu pedido',
+          icon: 'search',
+          link: '#',
+        },
+        {
+          title: 'Tenho um Cardápio',
+          caption: 'Cadastre seu cardápio e receba pedidos',
+          icon: 'menu_book',
+          link: '#',
+        },
+        {
+          title: 'Meus Pedidos',
+          caption: 'Acompanhe seus últimos pedidos',
+          icon: 'shopping_cart',
+          link: '#',
+        },
+        {
+          title: 'Entrar',
+          caption: 'Acesse sua conta',
+          icon: 'account_circle',
+          link: '#',
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ quasar/src/pages/Index.vue
@@ -0,0 +1,55 @@
+<template>
+  <q-page padding>
+    <div class="flex flex-center">
+      <img alt="Quasar logo" src="~assets/logo.png">
+    </div>
+
+    <!-- <div class="flex flex-center">
+      <h3 class="q-display-3">Escolha seu docinho</h3>
+    </div> -->
+
+    <div id="conteudo" class="row gutter-md">
+      <div class="col-xs-12 col-sm-4">
+        <q-card>
+          <q-title>
+            Usar minha localização atual
+          </q-title>
+          <q-separator/>
+          <q-card>
+            Próximo de...
+          </q-card>
+          <q-card-actions>
+            <q-btn to="list-restaurant" color="primary" label="Ver localização"/>
+          </q-card-actions>
+        </q-card>
+      </div>
+
+      <div class="col-xs-12 col-sm-4">Acessar a conta</div>
+      <div class="col-xs-12 col-sm-4">Listar localizações cadastradas</div>
+    </div>
+  </q-page>
+
+</template>
+
+<style>
+  .q-page img{
+    width: 10%;
+    height: auto;
+    margin-bottom: 30px;
+  }
+
+  #conteudo div {
+    text-align: center;
+  }
+
+  .q-card {
+    width: 50%;
+    height: auto;
+  }
+</style>
+
+<script>
+export default {
+  name: 'PageIndex',
+};
+</script>
